f347956abcf7b258e0e8305f13e95b67335abfa8
flatironschool <danliu277@gmail.com>
2020-01-28 16:23:35 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code here!
+document.querySelector('main#main').remove()
+let newHeader = document.createElement('H1')
+newHeader.innerHTML = 'Test is the champion'
+newHeader.id = 'victory'
+document.appendChild(newHeader)
