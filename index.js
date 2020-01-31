ce55d7c56c8fbd0928a8067fed1ae6f7bec01622
Ben Kagan <kagan.benjamin@gmail.com>
2020-01-31 13:10:12 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Write your code here!
+let main = document.querySelector('main#main');
+main.remove();
+const newHeader = document.createElement('h1');
+newHeader.className = 'victory';
+newHeader.id = 'victory';
+newHeader.innerHTML = 'Ben Kagan is the champion';
